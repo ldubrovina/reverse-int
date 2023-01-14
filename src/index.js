7f8f425c73f2d3a633dc6738d259141d9043242e
ldubrovina <dubrovinalv24@gmail.com>
2023-01-14 20:37:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let number = Math.abs(n)
+    let string = number.toString()
+    let array = string.split('')
+    let result = array.reverse()
+    let stringFirst = result.join('')
+    let resultSecond = Number(stringFirst)
+    return resultSecond;
 }
